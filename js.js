029d6e5754dab9e44860a3044f767191ad0c8ef9
Tiago sampaio <tiagodev1910@gmail.com>
2025-09-09 21:08:09 -0300

    att

--- js.js
+++ js.js
@@ -1,7 +1,39 @@
+let images = document.querySelectorAll()
+
+
 function clickOne(){
-    let btn1 = document.getElementsByClassName('txt')
-    let btn2 = document.getElementById('txt')
+ 
+    let btnOne = document.getElementById('txtOne')
+    
+    
+    if(btnOne.style.display == ''){
+        btnOne.style.display = 'block'
+    } else{
+        btnOne.style.display = 'none'
+    }
+    
+} function clickTwo(){
+    let btnTwo = document.getElementById('txtTwo')
 
-    btn1.style.display = "block"
+    if(btnTwo.style.display == 'none'){
+        btnTwo.style.display = 'block'
+    } else{
+        btnTwo.style.display = 'none'
+    }
+}   function clickThree(){
+     let btnThree = document.getElementById('txtThree')
 
+    if(btnThree.style.display == 'none'){
+        btnThree.style.display = 'block'
+    } else{
+        btnThree.style.display = 'none'
+    }
+}   function clickFour(){
+     let btnFour = document.getElementById('txtFour')
+
+    if(btnFour.style.display == 'none'){
+        btnFour.style.display = 'block'
+    } else{
+        btnFour.style.display = 'none'
+    }
 }
