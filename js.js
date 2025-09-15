d8ae5c3dddc0d9fac8ee04f1a4ce7c34c7833446
Tiago sampaio <tiagodev1910@gmail.com>
2025-09-15 11:14:20 -0300

    att

--- js.js
+++ js.js
@@ -1,49 +1,54 @@
+
 function clickOne(){
 
 let img1 = document.getElementById('img1')
 let btnOne = document.getElementById('txtOne')
 
 
-if(btnOne.style.display = 'none'){
+if(btnOne.style.display == ''){
     btnOne.style.display = 'block'
     img1.src = 'faq-accordion-main/assets/images/icon-minus.svg'
-} else if(btnOne.style.display = 'block'){
-    btnOne.style.display = 'none'
+} else{
+    btnOne.style.display = ''
     img1.src = 'faq-accordion-main/assets/images/icon-plus.svg'
 }
-
-
 }
-
-
-
-
 
 
 function clickTwo(){
     let btnTwo = document.getElementById('txtTwo')
+    let img2 = document.getElementById('img2')
     
 
     if(btnTwo.style.display == ''){
         btnTwo.style.display = 'block';
+        img2.src = 'faq-accordion-main/assets/images/icon-minus.svg'
         
     } else{
-        btnTwo.style.display = 'none'
+        btnTwo.style.display = ''
+        img2.src = 'faq-accordion-main/assets/images/icon-plus.svg'
     }
-}   function clickThree(){
-     let btnThree = document.getElementById('txtThree')
+}   
 
-    if(btnThree.style.display == 'none'){
+function clickThree(){
+     let btnThree = document.getElementById('txtThree')
+     let img3 = document.getElementById('img3')
+    if(btnThree.style.display == ''){
         btnThree.style.display = 'block'
+        img3.src = 'faq-accordion-main/assets/images/icon-minus.svg'
     } else{
-        btnThree.style.display = 'none'
+        btnThree.style.display = ''
+        img3.src = 'faq-accordion-main/assets/images/icon-plus.svg'
     }
 }   function clickFour(){
      let btnFour = document.getElementById('txtFour')
+     let img4 = document.getElementById('img4')
 
-    if(btnFour.style.display == 'none'){
+    if(btnFour.style.display == ''){
         btnFour.style.display = 'block'
+        img4.src = 'faq-accordion-main/assets/images/icon-minus.svg'
     } else{
-        btnFour.style.display = 'none'
+        btnFour.style.display = ''
+        img4.src = 'faq-accordion-main/assets/images/icon-plus.svg'
     }
 }
